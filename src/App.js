479f4e310a56fda013167fc2b710e052fb9002fc
Maksym <tigron1994@gmail.com>
2021-09-08 14:12:46 +0200

    обновил данные приложения

--- src/App.js
+++ src/App.js
@@ -1,25 +1,63 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import ContactForm from 'components/ContactForm';
+import ContactList from 'components/ContactList';
+import Filter from 'components/Filter';
+import s from 'App.module.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export class App extends Component {
+  state = {
+    contacts: [
+      { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+      { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+      { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+      { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+    ],
+    filter: '',
+  };
+
+  handleSubmit = e => {
+    if (this.state.contacts.some(contact => contact.name === e.name)) {
+      alert(`${e.name} is already in contacts.`);
+      return;
+    }
+    const contact = { name: e.name, number: e.number, id: Math.round(Math.random() * 10000) };
+    this.setState(prevState => ({
+      contacts: [contact, ...prevState.contacts],
+    }));
+  };
+
+  handleDeleteContact = e => {
+    this.setState({
+      contacts: this.state.contacts.filter(contact => {
+        return `${contact.id}` !== `${e.target.id}`;
+      }),
+    });
+  };
+
+  changeFilter = e => {
+    this.setState({ filter: e.target.value });
+  };
+
+  getFilteredContacts = () => {
+    const { contacts, filter } = this.state;
+
+    return contacts.filter(contact => contact.name.toLowerCase().includes(filter.toLowerCase()));
+  };
+
+  render() {
+    const { name, number, filter, contacts } = this.state;
+    const { handleSubmit, handleChange, handleDeleteContact, getFilteredContacts, changeFilter } =
+      this;
+    return (
+      <div>
+        <h2 className={s.title}>Phonebook</h2>
+        <ContactForm onSubmit={handleSubmit} name={name} number={number} onChange={handleChange} />
+        {contacts.length > 1 && <Filter filter={filter} onChange={changeFilter} />}
+        <h2 className={s.title}>Contacts</h2>
+        <ContactList contacts={getFilteredContacts()} onClick={handleDeleteContact} />
+      </div>
+    );
+  }
 }
 
 export default App;
